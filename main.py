2038139b2d8d6a5388890f3e7d6da941841dbe68
Michael Estrada <dev1@codeskills.com.co>
2023-02-16 08:23:25 -0500

    NT2 Se modifica main con f prints y se crea archivo de consiciones

---
+++ main.py
@@ -0,0 +1,16 @@
+#Datos Primitivos Quemados
+nombreUsuario = "Michael Estrada Colorado"
+edadUsuario = 32
+estaturaUsuario = 1.74
+yaDesayuno = True
+direccionUsuario = input("Digita tu dirección")
+num1 = int(input("Digita numero 1"))
+num2 = int(input("Digita numero 2"))
+suma = num1 + num2
+
+#Salidas en Consola
+
+print("Hola tu nombre es " + nombreUsuario + " Bienvenido")
+print("Tu esas es " , edadUsuario , " Estas viejito") 
+print(f"Tu direccion es {direccionUsuario} vive cerca")
+print(f"la suma de {num1} y {num2} es {num1 + num2}")
